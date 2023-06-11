fc3cd200c7fae856f0929cc2bb1467c324807b99
FadyFayezYounan <fadyfayez012@gmail.com>
2023-06-11 18:25:05 +0300

    add screenshots folder

--- lib/easy_date_timeline.dart
+++ lib/easy_date_timeline.dart
@@ -2,11 +2,3 @@ library easy_date_timeline;
 
 export 'src/widgets/easy_date_timeline_widget/easy_date_timeline_widget.dart';
 export 'src/properties/properties.dart';
-
-
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/package_image_1.jpg
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/easy_date_timeline.png
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/example_2.jpg
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/example_3.jpg
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/example_4.jpg
-//https://raw.githubusercontent.com/FadyFayezYounan/easy_date_timeline/master/screenshots/example_5.jpg
